2a68da0349534c82f0bb865823215f144570603e
João Santos <sanjoao@gmail.com>
2024-05-16 15:00:58 -0300

    Update script.js
    mais informações disponiveis

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('nivel_agua.json')  // Certifique-se de que o caminho está correto
+    fetch('nivel_agua.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok: ' + response.statusText);
@@ -7,16 +7,24 @@ document.addEventListener("DOMContentLoaded", function() {
             return response.json();
         })
         .then(data => {
-            const nivelAguaDiv = document.getElementById('nivel-agua');
-            if (data && data.nivel_agua) {
-                nivelAguaDiv.textContent = `Nível da água: ${data.nivel_agua} cm`;
+            const nivelAguaSaoGoncaloDiv = document.getElementById('nivel-agua-sao-goncalo');
+            const nivelAguaLagoaDosPatosDiv = document.getElementById('nivel-agua-lagoa-dos-patos');
+
+            if (data && data.nivel_agua_sao_goncalo) {
+                nivelAguaSaoGoncaloDiv.textContent = `Nível da água: ${data.nivel_agua_sao_goncalo} cm`;
             } else {
-                nivelAguaDiv.textContent = 'Erro ao carregar os dados. Dados incompletos ou mal formatados.';
+                nivelAguaSaoGoncaloDiv.textContent = 'Erro ao carregar os dados. Dados incompletos ou mal formatados para São Gonçalo.';
+            }
+
+            if (data && data.nivel_agua_lagoa_dos_patos) {
+                nivelAguaLagoaDosPatosDiv.textContent = `Nível da água: ${data.nivel_agua_lagoa_dos_patos} cm`;
+            } else {
+                nivelAguaLagoaDosPatosDiv.textContent = 'Erro ao carregar os dados. Dados incompletos ou mal formatados para Lagoa dos Patos.';
             }
         })
         .catch(error => {
             console.error('Fetch error:', error);
-            const nivelAguaDiv = document.getElementById('nivel-agua');
-            nivelAguaDiv.textContent = 'Erro ao carregar os dados. ' + error.message;
+            document.getElementById('nivel-agua-sao-goncalo').textContent = 'Erro ao carregar os dados. ' + error.message;
+            document.getElementById('nivel-agua-lagoa-dos-patos').textContent = 'Erro ao carregar os dados. ' + error.message;
         });
 });
