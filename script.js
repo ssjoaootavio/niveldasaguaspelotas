7349a0c5a6e225164ac453c39379b384fc5df317
João Santos <sanjoao@gmail.com>
2024-05-16 22:44:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Carregar os dados inicialmente
     loadData();
 
-    // Configurar o intervalo para recarregar a página a cada 30 minutos
     setInterval(function() {
         console.log("Recarregando a página para atualizar os dados...");
         window.location.reload(true);  // Força a recarga da página limpando o cache
-    }, 10000);  // 10000 milissegundos = 10 segundos
+    }, 1800000);  // 1800000 milissegundos = 30 minutos
 });
 
 function loadData() {
@@ -18,19 +16,23 @@ function loadData() {
             return response.json();
         })
         .then(data => {
+            const lastUpdatedDiv = document.getElementById('lastUpdated');
             const nivelAguaSaoGoncaloDiv = document.getElementById('nivel-agua-sao-goncalo');
             const nivelAguaLagoaDosPatosDiv = document.getElementById('nivel-agua-lagoa-dos-patos');
 
-            // Adicionando 9 cm ao nível da água do São Gonçalo antes de exibir
-            let nivelAguaSaoGoncalo = data.nivel_agua_sao_goncalo ? parseInt(data.nivel_agua_sao_goncalo) + 9 : null;
+            if (data.last_updated) {
+                lastUpdatedDiv.textContent = `Última atualização: ${new Date(data.last_updated).toLocaleString('pt-BR')}`;
+            } else {
+                lastUpdatedDiv.textContent = 'Última atualização: Dados não disponíveis.';
+            }
 
-            if (nivelAguaSaoGoncalo) {
-                nivelAguaSaoGoncaloDiv.textContent = `Nível da água: ${nivelAguaSaoGoncalo} cm`;
+            if (data.nivel_agua_sao_goncalo) {
+                nivelAguaSaoGoncaloDiv.textContent = `Nível da água: ${parseInt(data.nivel_agua_sao_goncalo) + 9} cm`;  // Adicionando 9 cm ao nível da água do São Gonçalo
             } else {
                 nivelAguaSaoGoncaloDiv.textContent = 'Erro ao carregar os dados. Dados incompletos ou mal formatados para São Gonçalo.';
             }
 
-            if (data && data.nivel_agua_lagoa_dos_patos) {
+            if (data.nivel_agua_lagoa_dos_patos) {
                 nivelAguaLagoaDosPatosDiv.textContent = `Nível da água: ${data.nivel_agua_lagoa_dos_patos} cm`;
             } else {
                 nivelAguaLagoaDosPatosDiv.textContent = 'Erro ao carregar os dados. Dados incompletos ou mal formatados para Lagoa dos Patos.';
@@ -38,7 +40,6 @@ function loadData() {
         })
         .catch(error => {
             console.error('Fetch error:', error);
-            document.getElementById('nivel-agua-sao-goncalo').textContent = 'Erro ao carregar os dados. ' + error.message;
-            document.getElementById('nivel-agua-lagoa-dos-patos').textContent = 'Erro ao carregar os dados. ' + error.message;
+            document.getElementById('lastUpdated').textContent = 'Erro ao carregar os dados: ' + error.message;
         });
 }
